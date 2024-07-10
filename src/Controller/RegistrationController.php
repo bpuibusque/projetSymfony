1a737be550c8463f3494600c617d69017cb6b1f9
bpuibusque <baptiste.puibusque@limayrac.fr>
2024-07-10 11:52:12 +0200

    commission end

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,7 +22,6 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
                     $userPasswordHasher->hashPassword(
                     $user,
@@ -33,7 +32,6 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
 
             return $security->login($user, 'form_login', 'main');
         }
