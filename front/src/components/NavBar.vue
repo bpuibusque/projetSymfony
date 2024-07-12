b4d4890ec931153400a29d0ecf5eaf9c488b287e
Gwena Bonnet <gwenael.bonnet@limayrac.fr>
2024-07-12 13:16:09 +0200

    ajout navbar css

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,6 +1,7 @@
-<template>
+<template> 
     <nav>
       <ul>
+        <img src="/logo-symfony.png" alt="Logo du site" id="site-logo">
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/commissions">Commissions</router-link></li>
         <li><router-link to="/private-messages">Private Messages</router-link></li>
@@ -27,47 +28,35 @@
   </script>
   
   <style>
-  nav ul {
+ nav ul {
     list-style-type: none;
     padding: 0;
     margin: 0;
     overflow: hidden;
-    background-color: #333;
-  }
-  nav ul li {
+    background-color: #25d366; /* Couleur de fond de la barre de navigation */
+}
+
+#site-logo {
+  height: 60px;
+  margin-right: 20px;
+  float: right;
+}
+
+nav ul li {
     float: left;
-  }
-  nav ul li a, .dropbtn {
+}
+
+nav ul li a, .dropbtn {
     display: inline-block;
     color: white;
     text-align: center;
-    padding: 14px 16px;
+    padding: 20px 20px;
     text-decoration: none;
-  }
-  nav ul li a:hover, .dropdown:hover .dropbtn {
-    background-color: #111;
-  }
-  nav ul li.dropdown {
-    display: inline-block;
-  }
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-  .dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-  }
-  .dropdown-content a:hover {background-color: #f1f1f1;}
-  .dropdown:hover .dropdown-content {
-    display: block;
-  }
+    font-weight: bold;
+}
+
+nav ul li a:hover, .dropdown:hover .dropbtn {
+    background-color: #128C7E; /* Couleur de fond au survol */
+}
   </style>
   
